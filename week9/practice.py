b1f9e78ee70e324b0723f87d9e389421b6040281
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-04-04 23:15:19 -0500

    python IV
    tcmg 272 wednesday class

---
+++ week9/practice.py
@@ -0,0 +1,114 @@
+
+# # ______________________________________________________________________slide 16
+# def printRecipe():
+#     print("1. Cook tortilla")
+#     print("2. Place cooked chicken onto tortilla.")
+#     print("3. Add your favorita salsa.")
+#     return("4. Enjoy!")
+
+# print(printRecipe())
+
+
+
+
+
+
+
+
+# def printRecipe(): # taco recipie
+#     print("1. Cook tortilla")
+#     print("2. Place cooked chicken onto tortilla.")
+#     print("3. Add your favorita salsa.")
+
+#     return("4. Enjoy!")
+
+# print(printRecipe())
+
+# def printRecipe(food):
+#     if (food == "taco"):
+#         print("1. Cook tortilla")
+#         print("2. Place cooked chicken onto tortilla.")
+#         print("3. Add your favorita salsa.")
+#     if (food == "ice cream"):
+#         print("1. Grab a bowl, spoon, your favorite ice cream, and toppings.")
+#         print("2. Scoop favorite ice cream flavor into bowl.")
+#         print("3. Add toppings.")
+#     return("4. Enjoy!")
+
+# print(printRecipe("ice cream"))
+
+
+
+
+def printRecipe(meal, desert):
+    if (meal == "taco"):
+        print("1. Cook tortilla")
+        print("2. Place cooked chicken onto tortilla.")
+        print("3. Add your favorita salsa.")
+    else: 
+        return("Meal recipe not available.")
+    
+    if (desert == "ice cream"):
+        print("1. Grab a bowl, spoon, your favorite ice cream, and toppings.")
+        print("2. Scoop favorite ice cream flavor into bowl.")
+        print("3. Add toppings.")
+    else: 
+        return ("Desert not available.")
+    return ("4. Enjoy!")
+
+print(printRecipe("taco", "ice cream"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ______________________________________________________________________slide 17
+# # right now I will be going over using a parameter
+# def printRecipe(food):
+#     if (food == "taco"):
+#         print("1. Cook tortilla")
+#         print("2. Place cooked chicken onto tortilla.")
+#         print("3. Add your favorita salsa.")
+#     if (food == "ice cream"):
+#         print("1. Grab a bowl, spoon, your favorite ice cream, and toppings.")
+#         print("2. Scoop favorite ice cream flavor into bowl.")
+#         print("3. Add toppings.")
+#     return("4. Enjoy!")
+
+# print(printRecipe("taco"))
+# # ______________________________________________________________________slide 18
+# def printRecipe(meal, desert):
+#     if (meal == "taco"):
+#         print("1. Cook tortilla")
+#         print("2. Place cooked chicken onto tortilla.")
+#         print("3. Add your favorita salsa.")
+#     else: 
+#         return("Meal recipe not available.")
+#     if (desert == "ice cream"):
+#         print("1. Grab a bowl, spoon, your favorite ice cream, and toppings.")
+#         print("2. Scoop favorite ice cream flavor into bowl.")
+#         print("3. Add toppings.")
+#     else: 
+#         return ("Desert not available.")
+#     return ("4. Enjoy!")
+
+# print(printRecipe("taco", "ice cream"))
+
---
+++ week9/wednesday-09.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+# def printRecipe():
+    print("1. Cook tortilla")
+    print("2. Place cooked chicken onto tortilla.")
+    print("3. Add your favorita salsa.")
+    return("4. Enjoy!")
+
+print(printRecipe())
